ed0364d45627e1af9913b794dedf59ef3735a002
Shrikant Pradhan <shripradhan.94@gmail.com>
2020-05-19 21:42:03 +0530

    Branch1 - Add save() method in RegistrationController

--- college-app-web/src/main/java/com/shree/collegeappweb/controller/RegistrationController.java
+++ college-app-web/src/main/java/com/shree/collegeappweb/controller/RegistrationController.java
@@ -1,12 +1,21 @@
 package com.shree.collegeappweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegistrationController {
 	
+	@RequestMapping(value = "/registration")
 	public String registrationForm() {
 		return "registration";
+	}
+	
+	@RequestMapping(value = "/registration", method = RequestMethod.POST )
+	public String save(@RequestBody Object object) {
+		return null;
 	}
 
 }
