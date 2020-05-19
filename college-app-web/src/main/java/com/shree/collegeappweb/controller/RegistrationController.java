4c8a23704aaa4a7b6417a05e0bb8666f7c89257b
acer <acer@DESKTOP-GAQF71N>
2020-05-19 21:37:13 +0530

    Branch1- Modify RegistrationController

--- college-app-web/src/main/java/com/shree/collegeappweb/controller/RegistrationController.java
+++ college-app-web/src/main/java/com/shree/collegeappweb/controller/RegistrationController.java
@@ -1,5 +1,12 @@
 package com.shree.collegeappweb.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class RegistrationController {
+	
+	public String registrationForm() {
+		return "registration";
+	}
 
 }
